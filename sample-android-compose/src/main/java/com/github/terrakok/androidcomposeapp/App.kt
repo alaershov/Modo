a0e4eee55b9e1c9d4ec9d9ec52e2d7ccf0793b3b
Alexey Ershov <alaershov@gmail.com>
2021-05-14 18:56:08 +0700

    add MultiReducer to Compose sample

--- sample-android-compose/src/main/java/com/github/terrakok/androidcomposeapp/App.kt
+++ sample-android-compose/src/main/java/com/github/terrakok/androidcomposeapp/App.kt
@@ -2,11 +2,12 @@ package com.github.terrakok.androidcomposeapp
 
 import android.app.Application
 import com.github.terrakok.modo.Modo
+import com.github.terrakok.modo.MultiReducer
 import com.github.terrakok.modo.android.compose.AppReducer
 import com.github.terrakok.modo.android.compose.LogReducer
 
 class App : Application() {
-    val modo = Modo(LogReducer(AppReducer(this)))
+    val modo = Modo(LogReducer(AppReducer(this, MultiReducer())))
 
     override fun onCreate() {
         INSTANCE = this
