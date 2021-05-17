49adf815623b9066692cb7a1ac82416223d2fdc4
Alexey Ershov <alaershov@gmail.com>
2021-05-17 13:09:34 +0700

    Use ModoRender composable in sample app

--- sample-android-compose/src/main/java/com/github/terrakok/androidcomposeapp/AppActivity.kt
+++ sample-android-compose/src/main/java/com/github/terrakok/androidcomposeapp/AppActivity.kt
@@ -3,35 +3,39 @@ package com.github.terrakok.androidcomposeapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import com.github.terrakok.modo.android.compose.ComposeRender
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.github.terrakok.modo.NavigationState
+import com.github.terrakok.modo.android.compose.ModoRender
 import com.github.terrakok.modo.android.compose.init
+import com.github.terrakok.modo.android.compose.observeAsState
 import com.github.terrakok.modo.android.compose.saveState
 import com.github.terrakok.modo.back
 
 class AppActivity : AppCompatActivity() {
+
     private val modo get() = App.INSTANCE.modo
-    private val render = ComposeRender(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         modo.init(savedInstanceState, Screens.Start())
+
         setContent {
             Surface(color = MaterialTheme.colors.background) {
-                render.Content()
+                val navigationState: NavigationState by modo.observeAsState()
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    NavigationStateConsole(navigationState)
+                    ModoRender(navigationState)
+                }
             }
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        modo.render = render
-    }
-
-    override fun onPause() {
-        super.onPause()
-        modo.render = null
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
