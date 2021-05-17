1ee6ac62aec2cd9cff5b4a7a27a2a82511d497b7
Alexey Ershov <alaershov@gmail.com>
2021-05-17 13:09:06 +0700

    Add simple widget for navigation state text representation. Not formatted yet.

---
+++ sample-android-compose/src/main/java/com/github/terrakok/androidcomposeapp/NavigationStateConsole.kt
@@ -0,0 +1,24 @@
+package com.github.terrakok.androidcomposeapp
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.github.terrakok.modo.NavigationState
+
+@Composable
+fun NavigationStateConsole(navigationState: NavigationState) {
+    Surface(color = Color.Black) {
+        Text(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            text = navigationState.toString(),
+            color = Color.White
+        )
+    }
+}
